0a3ae7cc8a77898b1ad23d37ef74855e3be68332
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-10-24 22:10:47 -0400

    Estableciendo los metodos crud para producto

--- src/main/java/com/jorge/demomarket/persistence/ProductoRepository.java
+++ src/main/java/com/jorge/demomarket/persistence/ProductoRepository.java
@@ -2,13 +2,35 @@ package com.jorge.demomarket.persistence;
 
 import com.jorge.demomarket.persistence.crud.ProductoCrudRepository;
 import com.jorge.demomarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository // Especificamos a springBoot que esta clase se encarga de interactuar con la BD
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProduct){
+        productoCrudRepository.deleteById(idProduct);
     }
 }
--- src/main/java/com/jorge/demomarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/jorge/demomarket/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,19 @@
 package com.jorge.demomarket.persistence.crud;
 
 import com.jorge.demomarket.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    // Consulta de manera nativa
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true )
+    List<Producto> getByCategoria(int idCategoria);
+
+    // Consulta usando Query Methods
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
