544a287e05f8335941714661e0f4cb451afb7aee
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-11-01 21:45:32 -0400

    Creando el modelo del dominio para compra

---
+++ src/main/java/com/jorge/demomarket/domain/Purchase.java
@@ -0,0 +1,70 @@
+package com.jorge.demomarket.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Purchase {
+    private int purchaseId;
+    private String clientid;
+    private LocalDateTime date;
+    private String paymentMethod;
+    private String comment;
+    private String state;
+    private List<PurchaseItem> item;
+
+    public int getPurchaseId() {
+        return purchaseId;
+    }
+
+    public void setPurchaseId(int purchaseId) {
+        this.purchaseId = purchaseId;
+    }
+
+    public String getClientid() {
+        return clientid;
+    }
+
+    public void setClientid(String clientid) {
+        this.clientid = clientid;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<PurchaseItem> getItem() {
+        return item;
+    }
+
+    public void setItem(List<PurchaseItem> item) {
+        this.item = item;
+    }
+}
---
+++ src/main/java/com/jorge/demomarket/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.jorge.demomarket.domain.repository;
+
+import com.jorge.demomarket.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
